153840a1de4458557e57aa22c6c5845d330fd20c
Jeremy <jeremytbradshaw@gmail.com>
2020-12-09 11:13:44 -0400

    Updates to MgPlayground

--- Dev/MSGraphPlayground.psm1
+++ Dev/MSGraphPlayground.psm1
@@ -24,9 +24,9 @@ function New-DeviceCodeAccessToken {
             "client_id=$($ApplicationId)",
             "scope=$($Scopes -join ' ')"
         ) -join '&'
-    
+
         $dcrParams = @{
-    
+
             Method      = 'POST'
             Uri         = "https://login.microsoftonline.com/$($Endpoint)/oauth2/v2.0/devicecode"
             Body        = $dcrBody
@@ -36,7 +36,7 @@ function New-DeviceCodeAccessToken {
         $dcrResponse = Invoke-RestMethod @dcrParams
     }
     catch { throw $_ }
-    
+
     $dtNow = [datetime]::Now
     $sw1 = [Diagnostics.Stopwatch]::StartNew()
     $dcExpiration = "$($dtNow.AddSeconds($dcrResponse.expires_in).ToString('yyyy-MM-dd hh:mm:ss tt'))"
@@ -50,7 +50,7 @@ function New-DeviceCodeAccessToken {
     # Wait for user to enter code before starting to poll token endpoint:
     switch (
         $host.UI.PromptForChoice(
-    
+
             "Authorization started (expires at $($dcExpiration)",
             "$($dcrResponse.message)",
             [ChoiceDescription]('&Done'),
@@ -72,7 +72,7 @@ function New-DeviceCodeAccessToken {
 
                 try {
                     $trParams = @{
-    
+
                         Method      = 'POST'
                         Uri         = "https://login.microsoftonline.com/$($Endpoint)/oauth2/v2.0/token"
                         Body        = $trBody
@@ -84,11 +84,11 @@ function New-DeviceCodeAccessToken {
                 }
                 catch {
                     if ($_.ErrorDetails.Message) {
-                        
+
                         $badResponse = ConvertFrom-Json -InputObject $_.ErrorDetails.Message
-    
+
                         if ($badResponse.error -eq 'authorization_pending') {
-                        
+
                             if ($pollCount -eq 1) {
 
                                 "The user hasn't finished authenticating, but hasn't canceled the flow (error: authorization_pending).  " +
@@ -141,25 +141,22 @@ function Get-RefreshedAcessToken {
         )]
         [ValidateScript(
             {
-                if ($_.token_type -eq 'Bearer' -and
-                    $_.scope -and
-                    $_.access_token -match '^[-\w]+\.[-\w]+\.[-\w]+$' -and
-                    $_.refreh_token -match '^[-\w]+\.[-\w]+\.[-\w]+$') { $true } else {
+                if ($_ -match '^0\.ASw[-\w]+\.[-\w]+$') { $true } else {
 
                     throw 'Invalid token object.  Supply $TokenObject where: $TokenObject = New-MSGraphAccessToken ...'
                 }
             }
         )]
-        [Object]$TokenObject
+        [string]$RefreshToken
     )
 
     $trBody = @(
         "grant_type=refresh_token",
-        "refresh_token=$($TokenObject.refresh_token)"
+        "refresh_token=$($RefreshToken)"
     ) -join '&'
 
     $trParams = @{
-    
+
         Method      = 'POST'
         Uri         = "https://login.microsoftonline.com/$($Endpoint)/oauth2/v2.0/token"
         Body        = $trBody
@@ -180,7 +177,7 @@ function New-AppOnlyAccessToken {
     )]
     param (
         [Parameter(Mandatory)]
-        [Alias('Tenant', 'TenantName', 'TenantDomain', 'TenantDomainName')]
+        [Alias('Tenant', 'TenantDomainName')]
         [string]$TenantId,
 
         [Parameter(Mandatory)]
@@ -202,7 +199,7 @@ function New-AppOnlyAccessToken {
         [ValidateScript(
             {
                 if (Test-Path -Path $_) { $true } else {
-                
+
                     throw "An example proper path would be 'cert:\CurrentUser\My\C3E7F30B9DD50B8B09B9B539BC41F8157642D317'."
                 }
             }
@@ -214,7 +211,6 @@ function New-AppOnlyAccessToken {
     )
 
     if ($PSCmdlet.ParameterSetName -eq 'CertificateStorePath') {
-
         try {
             $Script:Certificate = Get-ChildItem -Path $CertificateStorePath -ErrorAction Stop
         }
@@ -222,7 +218,7 @@ function New-AppOnlyAccessToken {
     }
     else { $Script:Certificate = $Certificate }
 
-    if (-not (Test-CertificateProvider -Certificate $Script:Certificate)) {
+    if (-not (Test-Certificate2 -Certificate $Script:Certificate)) {
 
         $ErrorMessage = "The supplied certificate does not use the provider 'Microsoft Enhanced RSA and AES Cryptographic Provider'.  " +
         "For best luck, use a certificate generated using New-SelfSignedMSGraphApplicationCertificate."
@@ -232,14 +228,15 @@ function New-AppOnlyAccessToken {
 
     $NowUTC = [datetime]::UtcNow
 
-    $JWTHeader = @{
+    $Header = @{
 
         alg = 'RS256'
         typ = 'JWT'
-        x5t = ConvertTo-Base64UrlFriendly -String ([Convert]::ToBase64String($Script:Certificate.GetCertHash()))
+        kid = ConvertTo-Base64Url -String ([Convert]::ToBase64String($Script:Certificate.GetCertHash()))
+        # x5t = ConvertTo-Base64Url -String ([Convert]::ToBase64String($Script:Certificate.GetCertHash()))
     }
 
-    $JWTClaims = @{
+    $Payload = @{
 
         aud = "https://login.microsoftonline.com/$TenantId/oauth2/token"
         exp = (Get-Date $NowUTC.AddMinutes($JWTExpMinutes) -UFormat '%s') -replace '\..*'
@@ -249,22 +246,19 @@ function New-AppOnlyAccessToken {
         sub = $ApplicationId.Guid
     }
 
-    $EncodedJWTHeader = [Convert]::ToBase64String(
-        
-        [Text.Encoding]::UTF8.GetBytes((ConvertTo-Json -InputObject $JWTHeader))
-    )
-    
-    $EncodedJWTClaims = [Convert]::ToBase64String(
-        
-        [Text.Encoding]::UTF8.GetBytes((ConvertTo-Json -InputObject $JWTClaims))
+    $EncodedHeader = [Convert]::ToBase64String(
+        [Text.Encoding]::UTF8.GetBytes((ConvertTo-Json -InputObject $Header))
     )
 
-    $JWT = ConvertTo-Base64UrlFriendly -String ($EncodedJWTHeader + '.' + $EncodedJWTClaims)
+    $EncodedPayload = [Convert]::ToBase64String(
+        [Text.Encoding]::UTF8.GetBytes((ConvertTo-Json -InputObject $Payload))
+    )
 
-    $Signature = ConvertTo-Base64UrlFriendly -String ([Convert]::ToBase64String(
-        
+    $JWT = ConvertTo-Base64Url -String ($EncodedHeader + '.' + $EncodedPayload)
+
+    $Signature = ConvertTo-Base64Url -String (
+        [Convert]::ToBase64String(
             $Script:Certificate.PrivateKey.SignData(
-            
                 [Text.Encoding]::UTF8.GetBytes($JWT),
                 [HashAlgorithmName]::SHA256,
                 [RSASignaturePadding]::Pkcs1
@@ -299,58 +293,167 @@ function New-AppOnlyAccessToken {
     $trResponse
 }
 
-function Test-CertificateProvider ([X509Certificate2]$Certificate) {
+function Test-Certificate2 ([X509Certificate2]$Certificate) {
 
     if ($PSVersionTable.PSEdition -eq 'Desktop') {
 
-        $certProvider = $Certificate.PrivateKey.CspKeyContainerInfo.ProviderName
+        $Provider = $Certificate.PrivateKey.CspKeyContainerInfo.ProviderName
     }
-    else { $certProvider = $Certificate.PrivateKey.Key.Provider }
+    else { $Provider = $Certificate.PrivateKey.Key.Provider }
 
-    if ($certProvider -eq 'Microsoft Enhanced RSA and AES Cryptographic Provider') {
-
+    if (
+        $Provider -eq 'Microsoft Enhanced RSA and AES Cryptographic Provider' -and
+        $Certificate.SignatureAlgorithm.FriendlyName -match '(sha256)'
+    ) {
         $true
     }
     else { $false }
 }
 
-function ConvertTo-Base64Url ([string]$String) {
+function ConvertTo-Base64Url {
+
+    [ValidatePattern('^(?:[A-Za-z0-9+\/]{4})*(?:[A-Za-z0-9+\/]{2}==|[A-Za-z0-9+\/]{3}=|[A-Za-z0-9+\/]{4})$')]
+    [string]$String
 
     $String -replace '\+', '-' -replace '/', '_' -replace '='
 }
+function ConvertFrom-Base64Url ([string[]]$String) {
 
-function ConvertFrom-Base64Url ([string]$String) {
+    foreach ($s in $String) {
 
+    }
     while ($String.Length % 4) { $String += '=' }
     $String -replace '-', '\+' -replace '_', '/'
 }
 
-function ConvertFrom-JWT {
+function ConvertFrom-JWTAccessToken {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory)]
         [ValidateScript(
             {
-                if ($_ -match '^eyJ0[-\w]+\.[-\w]+\.[-\w]+$') { $true } else { throw 'Invalid JWT.' }
+                if ($_ -match '^eyJ[-\w]+\.[-\w]+\.[-\w]+$') { $true } else { throw 'Invalid JWT.' }
             }
         )]
         [Object]$JWT
     )
 
-    $Headers, $Claims = ($JWT -split '\.')[0,1]
-    
+    $Headers, $Claims = ($JWT -split '\.')[0, 1]
+
     [PSCustomObject]@{
+        Headers = ConvertFrom-Json (
+            [Text.Encoding]::ASCII.GetString(
+                [Convert]::FromBase64String((ConvertFrom-Base64Url $Headers))
+            )
+        )
+        Payload = ConvertFrom-Json(
+            [Text.Encoding]::ASCII.GetString(
+                [Convert]::FromBase64String((ConvertFrom-Base64Url $Claims))
+            )
+        )
+    }
+}
 
-        Headers = [System.Text.Encoding]::ASCII.GetString(
+function New-MSGraphRequest2 {
 
-            [Convert]::FromBase64String((ConvertFrom-Base64Url $Headers))
-        ) |
-        ConvertFrom-Json
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory)]
+        [Alias('Query')]
+        [string]$Request,
 
-        Claims = [System.Text.Encoding]::ASCII.GetString(
-        
-            [Convert]::FromBase64String((ConvertFrom-Base64Url $Claims))
-        ) |
-        ConvertFrom-Json
+        [Parameter(Mandatory)]
+        # [ValidateScript(
+        #     {
+        #         if ($_.token_type -eq 'Bearer' -and $_.access_token -match '^[-\w]+\.[-\w]+\.[-\w]+$') { $true } else {
+
+        #             throw 'Invalid access token.  For best results, supply $AccessToken where: $AccessToken = New-MSGraphAccessToken ...'
+        #         }
+        #     }
+        # )]
+        [Object]$AccessToken,
+
+        [Alias('API', 'Version', 'Endpoint')]
+        [ValidateSet('v1.0', 'beta')]
+        [string]$ApiVersion = 'v1.0',
+
+        [ValidateSet('GET', 'POST', 'PATCH', 'PUT', 'DELETE')]
+        [string]$Method = 'GET',
+
+        [string]$Body,
+
+        [ValidateSet('Warn', 'Inquire', 'Continue', 'SilentlyContinue')]
+        [string]$nextLinkAction = 'Warn'
+    )
+
+    $RequestParams = @{
+
+        Headers     = @{ Authorization = "Bearer $($AccessToken.access_token)" }
+        Uri         = "https://graph.microsoft.com/$($ApiVersion)/$($Request)"
+        Method      = $Method
+        ContentType = 'application/json'
+        ErrorAction = 'Stop'
+    }
+
+    if ($PSBoundParameters.ContainsKey('Body')) {
+
+        if ($Method -notmatch '(POST)|(PATCH)') {
+
+            throw "Body is not allowed when the method is $($Method), only POST or PATCH."
+        }
+        else { $RequestParams['Body'] = $Body }
+    }
+
+    try {
+        Invoke-RestMethod @RequestParams -OutVariable requestResponse
+    }
+    catch { throw $_ }
+
+    if ($requestResponse.'@odata.nextLink') {
+
+        $Script:Continue = $true
+
+        switch ($nextLinkAction) {
+
+            Warn {
+                Write-Warning -Message "There are more results available. Next page: $($requestResponse.'@odata.nextLink')"
+                $Script:Continue = $false
+            }
+            Continue {
+                Write-Information -MessageData 'There are more results available.  Getting the next page' -InformationAction Continue
+
+            }
+            Inquire {
+                switch (
+                    $host.UI.PromptForChoice(
+
+                        'There are more results available (i.e. response included @odata.nextLink).',
+                        'Get more results?',
+                        [ChoiceDescription[]]@('&Yes', 'Yes to &All', '&No'),
+                        2
+                    )
+                ) {
+                    0 {} # Will prompt for choice again if the next response includes another @odata.nextLink.
+                    1 { $nextLinkAction = 'SilentlyContinue' }
+                    2 { $Script:Continue = $false }
+                }
+            }
+        }
+
+        if ($Script:Continue) {
+
+            $nextLinkRequestParams = @{
+                AccessToken    = $AccessToken
+                ApiVersion     = $ApiVersion
+                Request        = "$($requestResponse.'@odata.nextLink' -replace 'https://graph.microsoft.com/(v1\.0|beta)/')"
+                nextLinkAction = $nextLinkAction
+                ErrorAction    = 'Stop'
+            }
+
+            try {
+                New-MSGraphRequest @nextLinkRequestParams
+            }
+            catch { throw $_ }
+        }
     }
 }
