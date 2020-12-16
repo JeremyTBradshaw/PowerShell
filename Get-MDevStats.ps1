c531e34d063bf77272560bbadb6c780714941d22
Jeremy <jeremytbradshaw@gmail.com>
2020-12-16 09:43:50 -0400

    Update Get-MDevStats.ps1

--- Get-MDevStats.ps1
+++ Get-MDevStats.ps1
@@ -1,96 +1,99 @@
 <#
     .Synopsis
     Get mobile devices and their statistics for all on-premises mailboxes, for use with mailbox migration planning.
+
+    .Example
+    .\Get-MDevStats.ps1 | Export-Csv "MDevStats_$(Get-Date -Format 'yyyy-MM-dd').csv" -NTI -Encoding UTF8
 #>
 #Requires -Version 5.1
 [CmdletBinding()]
 param()
 
-begin {
-    # Use Exchange 2016 Management Shell or remote PowerShell from Windows 10 to an Exchange 2016 server.
-    $PSSessionsByComputerName = Get-PSSession | Group-Object -Property ComputerName
-    if (-not (Get-Command Get-MobileDeviceStatistics)) {
-        
-        Write-Warning -Message "Command 'Get-MobileDeviceStatistics' is not available.  Make sure to run this script against Exchange 2016 or newer."
-        break
-    }
-    elseif ($PSSessionsByComputerName.Name -eq 'outlook.office365.com') {
+#region Initialization
+$PSSessionsByComputerName = Get-PSSession | Group-Object -Property ComputerName
+if (-not (Get-Command Get-MobileDeviceStatistics)) {
 
-        Write-Warning -Message 'EXO PSSession detected.  This script is intended for use with on-premises Exchange.  Exiting script.'
-    }
+    "Command 'Get-MobileDeviceStatistics' is not available.  Make sure to run this script against Exchange 2016 or newer." |
+    Write-Warning
+    break
+}
+elseif ($PSSessionsByComputerName.Name -eq 'outlook.office365.com') {
 
-    $Start = [datetime]::Now
-    $ProgressProps = @{
-
-        Activity        = "Get-MailboxReport - Start time: $($Start)"
-        Status          = 'Working'
-        PercentComplete = -1
-    }
-
-    try {
-        Write-Progress @ProgressProps -CurrentOperation 'Get-Mailbox (on-premises mailboxes)'
-        $LocalMailboxes = Get-Mailbox -ResultSize Unlimited -ErrorAction Stop |
-        Where-Object { $_.RecipientTypeDetails -ne 'DiscoveryMailbox' -and $_.RecipientTypeDetails -ne 'ArbitrationMailbox' }
-    }
-    catch {
-        Write-Warning -Message "Failed on initial data collection step.  Exiting script.  Error`n`n($_.Exception)"
-        break
-    }
+    'EXO PSSession detected.  This script is intended for use with on-premises Exchange.  Exiting script.' |
+    Write-Warning
+    break
 }
 
-process {
-    # Start the main loop:
+$Start = [datetime]::Now
+$ProgressProps = @{
 
-    $ProgressCounter = 0
-    $Stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
+    Activity        = "Get-MDevStats.ps1 - Start time: $($Start)"
+    Status          = 'Working'
+    PercentComplete = -1
+}
 
-    foreach ($lm in $LocalMailboxes) {
+try {
+    Write-Progress @ProgressProps -CurrentOperation 'Get-Mailbox (on-premises mailboxes)'
+    $LocalMailboxes = Get-Mailbox -ResultSize Unlimited -ErrorAction Stop |
+    Where-Object { $_.RecipientTypeDetails -ne 'DiscoveryMailbox' -and $_.RecipientTypeDetails -ne 'ArbitrationMailbox' }
+}
+catch {
+    Write-Warning -Message "Failed on initial Get-Mailbox step.  Exiting script.  Error`n`n($_.Exception)"
+    break
+}
+#endregion Initialization
 
-        $ProgressCounter++
-        if ($Stopwatch.Elapsed.Milliseconds -ge 300) {
+#region Main loop
+$ProgressCounter = 0
+$Stopwatch = [System.Diagnostics.Stopwatch]::StartNew()
 
-            $ProgressProps['PercentComplete'] = (($ProgressCounter / $LocalMailboxes.Count) * 100)
-            $ProgressProps['CurrentOperation'] = "Preparing user/device custom objects for $($lm.DisplayName) ($($lm.PrimarySmtpAddress))"
-            Write-Progress @ProgressProps
+foreach ($mbx in $LocalMailboxes) {
 
-            $Stopwatch.Restart()
-        }
+    $ProgressCounter++
+    if ($Stopwatch.Elapsed.Milliseconds -ge 300) {
 
-        $MDevs = @()
-        $MDevs += Get-MobileDeviceStatistics -Mailbox $lm.Guid.Guid -ErrorAction SilentlyContinue |
-        Select-Object -Property @{
-            Name       = 'UserDisplayName'
-            Expression = { $lm.DisplayName }
-        },
-        LastSuccessSync,
-        DeviceAccessState,
-        DeviceAccessStateReason,
-        Status,
-        DeviceModel,
-        DeviceImei,
-        DevicePhoneNumber,
-        DeviceOS,
-        DeviceType,
-        DeviceID,
-        DeviceUserAgent,
-        DeviceFriendlyName,
-        DeviceMobileOperator,
-        DevicePolicyApplied,
-        DevicePolicyApplicationStatus,
-        LastDeviceWipeRequestor,
-        ClientVersion,
-        NumberOfFoldersSynced,
-        ClientType,
-        @{
-            Name       = 'UserGuid'
-            Expression = { $lm.Guid.Guid }
-        },
-        Guid
+        $ProgressProps['PercentComplete'] = (($ProgressCounter / $LocalMailboxes.Count) * 100)
+        $ProgressProps['CurrentOperation'] = "Preparing user/device custom objects for $($mbx.DisplayName) ($($mbx.PrimarySmtpAddress))"
+        Write-Progress @ProgressProps
 
-        if ($MDevs.Count -ge 1) {
+        $Stopwatch.Restart()
+    }
 
-            # Output the combined user/device objects:
-            $MDevs
-        }
+    $MDevs = @()
+    $MDevs += Get-MobileDeviceStatistics -Mailbox $mbx.Guid.Guid -ErrorAction SilentlyContinue |
+    Select-Object -Property @{
+        Name       = 'UserDisplayName'
+        Expression = { $mbx.DisplayName }
+    },
+    LastSuccessSync,
+    DeviceAccessState,
+    DeviceAccessStateReason,
+    Status,
+    DeviceModel,
+    DeviceImei,
+    DevicePhoneNumber,
+    DeviceOS,
+    DeviceType,
+    DeviceID,
+    DeviceUserAgent,
+    DeviceFriendlyName,
+    DeviceMobileOperator,
+    DevicePolicyApplied,
+    DevicePolicyApplicationStatus,
+    LastDeviceWipeRequestor,
+    ClientVersion,
+    NumberOfFoldersSynced,
+    ClientType,
+    Guid,
+    @{
+        Name = 'UserUPN'
+        Expression = {$mbx.UserPrincipalName}
+    }
+
+    if ($MDevs.Count -ge 1) {
+
+        # Output the combined user/device objects:
+        $MDevs
     }
 }
+#endregion Main loop
