249991c62f18675acfdddf1b6ccaf29386897e04
Jeremy <jeremytbradshaw@gmail.com>
2021-04-14 15:15:45 -0300

    Added some housekeeping.

--- Remove-ExchangeLogs.ps1
+++ Remove-ExchangeLogs.ps1
@@ -1,20 +1,23 @@
 #Requires -Version 4
 
 $MaxLogFileAge = 30
+$MaxCleanupLogFileAge = 90
+
+$DateTime = [DateTime]::Now
+
 $ExLogFolderPaths = @(
 
-    'F:\inetpub\logs\LogFiles\',
+    'C:\inetpub\logs\LogFiles\',
     'C:\Program Files\Microsoft\Exchange Server\V15\Logging\',
     'C:\Program Files\Microsoft\Exchange Server\V15\Bin\Search\Ceres\Diagnostics\ETLTraces\',
     'C:\Program Files\Microsoft\Exchange Server\V15\Bin\Search\Ceres\Diagnostics\Logs'
 )
 
+
+$LogFolder = Join-Path -Path $PSScriptRoot -ChildPath 'Logging_Remove-ExchangeLogs.ps1'
+if (-not (Test-Path -Path $LogFolder)) { New-Item -Path $LogFolder -ItemType Directory }
+
 function Write-Log ([string]$Message) {
-
-    $DateTime = [DateTime]::Now
-
-    $LogFolder = Join-Path -Path $PSScriptRoot -ChildPath 'Logging_Remove-ExchangeLogs.ps1'
-    if (-not (Test-Path -Path $LogFolder)) { New-Item -Path $LogFolder -ItemType Directory }
 
     $LogFile = Join-Path -Path $LogFolder -ChildPath "Exchange-Log-Cleanup_$($DateTime.ToString('yyyy-MM-dd')).log"
     if (-not (Test-Path -Path $LogFile)) { New-Item -Path $LogFile -ItemType:File }
@@ -23,18 +26,16 @@ function Write-Log ([string]$Message) {
 
     if ($PSVersionTable.PSVersion.Major -eq 4) { Write-Host -Object $MessageData }
     else { Write-Information -MessageData $MessageData -InformationAction Continue }
-    
+
     $MessageData | Out-File -FilePath $LogFile -Append
 }
 
 function Remove-ExchangeLogs ([System.IO.FileInfo[]]$ExLogFolderPaths, [int]$MaxLogFileAge) {
 
-    $DateTime = [DateTime]::Now
-
     Write-Log -Message "Remove-ExchangeLog.ps1 starting: MaxLogFileAge = $($MaxLogFileAge), Folders to process:`r`n`t$($ExLogFolderPaths -join ""`r`n`t"")"
 
     foreach ($Folder in $ExLogFolderPaths) {
-        
+
         Write-Log -Message "Processing folder: $($Folder)"
 
         if (Test-Path $Folder) {
@@ -61,4 +62,26 @@ function Remove-ExchangeLogs ([System.IO.FileInfo[]]$ExLogFolderPaths, [int]$Max
     Write-Log -Message 'Remove-ExchangeLog.ps1 finished/ending.'
 }
 
+function Remove-CleanupLogs ([int]$MaxCleanupLogFileAge) {
+
+    Write-Log -Message "Remove-CleanupLog.ps1 starting: MaxCleanupLogFileAge = $($MaxCleanupLogFileAge)"
+
+    $CleanupLogsToDelete = Get-ChildItem -Path $Folder -Recurse -Include 'Exchange-Log-Cleanup_*.log' |
+    Where-Object {$_.LastWriteTime -le $DateTime.AddDays(-$MaxCleanupLogFileAge)}
+
+    foreach ($CleanupLog in $CleanupLogsToDelete) {
+
+        try {
+            Remove-Item $CleanupLog.FullName -ErrorAction Stop | Out-Null
+            Write-Log -Message "[Housekeeping] Successfully deleted '$($CleanupLog.FullName)'."
+        }
+        catch {
+            Write-Log -Message "[Housekeeping] Failed to delete '$($CleanupLog.FullName)'.`r`n`tException:`r`n$($_.Exception)"
+        }
+    }
+
+    Write-Log -Message 'Remove-CleanupLog.ps1 finished/ending.'
+}
+
 Remove-ExchangeLogs -ExLogFolderPaths $ExLogFolderPaths -MaxLogFileAge $MaxLogFileAge
+Remove-CleanupLogs -MaxCleanupLogFileAge $MaxCleanupLogFileAge
