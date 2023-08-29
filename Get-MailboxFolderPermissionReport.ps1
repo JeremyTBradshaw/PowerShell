a2501d4e2548d42b6c4ab62f831804caf213754f
Jeremy <Jeremy.Bradshaw@outlook.com>
2023-08-28 21:57:08 -0300

    TLC for an oldie + meh

---
+++ Get-MailboxFolderPermissionReport.ps1
@@ -0,0 +1,140 @@
+<#
+    .Synopsis
+    Get a report of assigned mailbox folder permissions for a given mailbox.
+
+    .Parameter Identity
+    Treat this parameter exactly like Get-MailboxFolderStatistics cmdlet's -Identity parameter.
+
+    .Parameter ReportMode
+    Summary mode outputs one permission object per folder, with columns for each permission role.  Detailed mode,
+    the default, outputs one permission object for every access right entry for every folder.
+
+    .Parameter ExportCSVs
+#>
+[CmdletBinding(DefaultParameterSetName = 'NoExport')]
+Param (
+    [Parameter(ParameterSetName = 'NoExport', Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+    [Parameter(ParameterSetName = 'ExportCSVs', Mandatory, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+    [string]$Identity,
+
+    [Parameter(ParameterSetName = 'NoExport')]
+    [ValidateSet('Summary', 'Detailed')]
+    [string]$ReportMode = 'Detailed',
+
+    [Parameter(ParameterSetName = 'NoExport')]
+    [Parameter(ParameterSetName = 'ExportCSVs')]
+    [Object]$ResultSize = 'Unlimited',
+
+    [Parameter(ParameterSetName = 'ExportCSVs')]
+    [switch]$ExportCSVs,
+
+    [Parameter(ParameterSetName = 'ExportCSVs')]
+    [ValidateScript(
+        {
+            if (Test-Path -Path $_ -PathType Container) { $true }
+            else { throw "'$($_)' does not appear to be valid folder path (absolute or relative)." }
+        }
+    )]
+    [System.IO.FileInfo]$OutputFolderPath,
+
+    [bool]$ShowIEQDuplicateFolderIds = $true
+)
+begin {
+    if (-not (Get-Command Get-MailboxFolderStatistics, Get-MailboxFolderPermission -ea SilentlyContinue)) {
+
+        throw 'This script requires an active session to Exchange/EXO and access to the Get-MailboxFolderStatistics and Get-MailboxFolderPermission cmdlets.'
+    }
+    $dtNow = [datetime]::Now
+    $Progress = @{
+
+        Activity        = "$($PSCmdlet.MyInvocation.MyCommand.Name) -Start time: $($dtNow)"
+        PercentComplete = -1
+    }
+
+    $_outputFolder = if ($PSCmdlet.MyInvocation.BoundParameters.ContainsKey('OutputFolderPath')) { $OutputFolderPath }
+    else { $PWD }
+}
+process {
+    try {
+        # The following line gets around an issue with ExchangeOnlineManagement PS module v3.0.0 where $ProgressPreference is repeatedly set to 'SilentlyContinue'
+        $ProgressPreference = 'Continue'
+        Write-Progress @Progress -CurrentOperation "Get-MailboxFolderStatistics -Identity '$($Identity)'"
+
+
+        $MailboxFolderStatistics = Get-MailboxFolderStatistics -Identity $Identity -ResultSize:$ResultSize -ea Stop
+
+        if ($ShowIEQDuplicateFolderIds) {
+            # Warn on case-insensitive duplidate FolderId's which can be problematic if setting permissions using FolderId for the -Identity parameter:
+            $_dupeFolderIds = @($MailboxFolderStatistics | Group-Object -Property FolderId | Where-Object { $_.Count -ge 2 })
+            if ($_dupeFolderIds.Count -ge 1) {
+
+                "The following folders have duplicate (matching) FolderId's, which means they should only be referenced by folder path, not FolderId, " +
+                'for the -Identity parameter of Get-/Set-/Add-/Remove-MailboxFolderPermission cmdlets: ' | Write-Warning
+
+                foreach ($_dupe in ($_dupeFolderIds | Sort-Object -Property Name)) {
+
+                    Write-Warning "$($_dupe.Name) ($($_dupe.Count) folders sharing same FolderId (case-insensitive))."
+                }
+            }
+        }
+
+        if ($PSCmdlet.ParameterSetName -eq 'ExportCSVs') {
+            $SummaryCSVFile = New-Item -Path $_outputFolder -Name "MailboxFolderPermissionReport_Summary_$([datetime]::Now.ToString('yyyy-MM-dd_hhmmttzz')).csv" -ItemType File -ea Stop
+            $DetailedCSVFile = New-Item -Path $_outputFolder -Name "MailboxFolderPermissionReport_Detailed_$([datetime]::Now.ToString('yyyy-MM-dd_hhmmttzz')).csv" -ItemType File -ea Stop
+        }
+
+        $_folderCounter = 0
+        foreach ($folder in $MailboxFolderStatistics) {
+            try {
+                $_folderCounter++
+
+                $Progress['PercentComplete'] = (($_folderCounter / $MailboxFolderStatistics.Count) * 100)
+                # The following line gets around an issue with ExchangeOnlineManagement PS module v3.0.0 where $ProgressPreference is repeatedly set to 'SilentlyContinue'
+                $ProgressPreference = 'Continue'
+                Write-Progress @Progress -Status 'Getting permissions...' -CurrentOperation "Current folder: $($folder.FolderPath)"
+
+                $Permissions = Get-MailboxFolderPermission -Identity "$($Identity):$($folder.FolderId)" -ea Stop
+            }
+            catch {
+                Write-Warning -Message "Failed to get permissions for FolderPath: $($folder.FolderPath)"
+            }
+
+            if (($ReportMode -eq 'Summary') -or ($PSCmdlet.ParameterSetName -eq 'ExportCSVs')) {
+                $Summary = [PSCustomObject] @{
+                    'Folder'           = $folder.FolderPath
+                    'None'             = ($Permissions | Where-Object { $folder.AccessRights -eq 'None' }).User -join ', '
+                    'Custom'           = ($Permissions | Where-Object { ($folder.AccessRights -like '*') -and ($folder.AccessRights -ne 'None') -and ($folder.AccessRights -ne 'Owner') -and ($folder.AccessRights -ne 'PublishingEditor') -and ($folder.AccessRights -ne 'Editor') -and ($folder.AccessRights -ne 'PublishingAuthor') -and ($folder.AccessRights -ne 'Author') -and ($folder.AccessRights -ne 'NonEditingAuthor') -and ($folder.AccessRights -ne 'Reviewer') -and ($folder.AccessRights -ne 'Contributor') }).User -join ', '
+                    'Owner'            = ($Permissions | Where-Object { $folder.AccessRights -eq 'Owner' }).User -join ', '
+                    'PublishingEditor' = ($Permissions | Where-Object { $folder.AccessRights -eq 'PublishingEditor' }).User -join ', '
+                    'Editor'           = ($Permissions | Where-Object { $folder.AccessRights -eq 'Editor' }).User -join ', '
+                    'PublishingAuthor' = ($Permissions | Where-Object { $folder.AccessRights -eq 'PublishingAuthor' }).User -join ', '
+                    'Author'           = ($Permissions | Where-Object { $folder.AccessRights -eq 'Author' }).User -join ', '
+                    'NonEditingAuthor' = ($Permissions | Where-Object { $folder.AccessRights -eq 'NonEditingAuthor' }).User -join ', '
+                    'Reviewer'         = ($Permissions | Where-Object { $folder.AccessRights -eq 'Reviewer' }).User -join ', '
+                    'Contributor'      = ($Permissions | Where-Object { $folder.AccessRights -eq 'Contributor' }).User -join ', '
+                    'MailboxIdentity'  = $Identity
+                    'FolderId'         = $folder.FolderId
+                }
+                if ($ReportMode -eq 'Summary') { $Summary }
+                else { $Summary | Export-Csv -Path $SummaryCSVFile -Append -NTI -Encoding utf8 -ea Stop }
+            }
+
+            if (($ReportMode -eq 'Detailed') -or ($PSCmdlet.ParameterSetName -eq 'ExportCSVs')) {
+                foreach ($perm in $Permissions) {
+                    [PSCustomObject]@{
+                        Folder          = $folder.FolderPath
+                        User            = $perm.User
+                        AccessRights    = $perm.AccessRights
+                        SharingFlags    = $perm.SharingFlags -join ', '
+                        MailboxIdentity = $Identity
+                        FolderId        = $folder.FolderId
+                    } |
+                    Tee-Object -Variable _detailedOutput
+                    if ($PSCmdlet.ParameterSetName -eq 'ExportCSVs') { $_detailedOutput | Export-Csv -Path $DetailedCSVFile -Append -NTI -Encoding utf8 -ea Stop }
+                }
+            }
+        }
+    }
+    catch { throw }
+}
+end { Write-Progress @Progress -Completed }
--- Get-MessageTrackingLogs.ps1
+++ Get-MessageTrackingLogs.ps1
@@ -8,11 +8,45 @@
 [CmdletBinding()]
 param (
     [string[]]$Recipients,
+    [Parameter(Mandatory, HelpMessage = 'I insist.')]
     [datetime]$Start#,
     #Remaining parameters to come soon.
 )
 begin {
     Write-Warning -Message '2023-08-11: Script is not complete, not intended for use, etc.'
+
+    # Eventually will use the following to enable all of the original parameters for pass-through:
+    $cmdGetMsgTrkLog = Get-Command Get-MessageTrackingLog
+    $cmdGetMsgTrkLog.Parameters.Values | ForEach-Object {
+        $_ | Select-Object Name,
+        @{
+            Name       = 'Type'
+            Expression = { $_.ParameterType.Name }
+        },
+        IsDynamic, SwitchParameter,
+        @{
+            Name       = 'Aliases'
+            Expression = { $_.Aliases -join ', ' }
+        },
+        @{
+            Name       = 'Mandatory'
+            Expression = { $_.Attributes.Mandatory }
+        },
+        @{
+            Name       = 'HelpMessage'
+            Expression = { $_.Attributes.HelpMessage }
+        },
+        @{
+            Name       = 'ValueFromPipeline'
+            Expression = { $_.Attributes.ValueFromPipeline }
+        },
+        @{
+            Name       = 'ValueFromPipelineByPropertyName'
+            Expression = { $_.Attributes.ValueFromPipelineByPropertyName }
+        }
+    }
+
+    # Will loop through these servers' MsgTrkLog's:
     $TS = Get-TransportService
 }
 process {}
